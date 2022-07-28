c714ad8141881e645f63eb33a004bff1cc54d258
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-07-28 16:08:04 -0600

    comments located

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
+
 
 module.exports = () => {
   return {
@@ -17,7 +17,7 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-
+    // Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
@@ -48,7 +48,7 @@ module.exports = () => {
         ],
       }),
     ],
-
+// Add CSS loaders and babel to webpack.
     module: {
       rules: [
         {
